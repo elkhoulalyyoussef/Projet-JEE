a0daaa8867ce7e8defd895d877fa5cf56177319b
elkhoulaly youssef <elkhoulalyyoussef@gmail.com>
2021-11-30 10:27:01 +0100

    Add files via upload

---
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -0,0 +1,31 @@
+package org.sid.billingservice;
+import org.sid.billingservice.feign.CustomerRestClient;
+import org.sid.billingservice.feign.ProductItemRestClient;
+import org.sid.billingservice.model.Customer;
+import org.sid.billingservice.repository.BillRepository;
+import org.sid.billingservice.repository.ProductItemRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+@EnableFeignClients
+public class BillingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(BillRepository billRepository,ProductItemRepository productItemRepositor , CustomerRestClient customerRestClient,
+                            ProductItemRestClient productItemRestClient){
+        return  args -> {
+            System.out.println("start");
+           Customer customer=customerRestClient.getCustomerById(1L);
+           System.out.println("---------------------------------");
+           System.out.println(customer.getId());
+           System.out.println(customer.getName());
+           System.out.println(customer.getEmail());
+
+        };
+    }
+}
